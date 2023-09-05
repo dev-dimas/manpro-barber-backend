283599f58ccb5a2ca4247411cfe051687f2c8f24
Dimas Maulana <75769207+dev-dimas@users.noreply.github.com>
2023-09-05 18:31:24 +0700

    conf: setup config for database, swagger, eslint

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Barber App')
+    .setDescription('Complete documentation for barber app')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('/', app, document, {
+    customSiteTitle: 'Barber App API',
+  });
+
+  app.setGlobalPrefix('api');
   await app.listen(3000);
 }
 bootstrap();
