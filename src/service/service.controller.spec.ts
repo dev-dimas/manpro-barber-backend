aa3e5e42363d44edc30b97a6275b09c27eebeebd
Ridwan Efendi <ridwanefendi1903@gmail.com>
2023-10-07 21:49:55 +0700

    crud service
    belum di testing lewat postman/swagger

--- src/service/service.controller.spec.ts
+++ src/service/service.controller.spec.ts
@@ -1,15 +1,37 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ServiceController } from './service.controller';
+import { EmployeeRepository } from '../employee/repository/employee.repository';
+import { ServiceRepository } from './repository/service.repository';
+import { ServiceService } from './service.service';
 
 describe('ServiceController', () => {
   let controller: ServiceController;
+  let service: ServiceService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ServiceController],
+      providers: [
+        ServiceService,
+        {
+          provide: ServiceRepository,
+          useValue: {
+            addService: jest.fn(),
+          },
+        },
+        {
+          provide: EmployeeRepository,
+          useValue: {
+            getEmployeeById: jest.fn(),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<ServiceController>(ServiceController);
+    service = module.get<ServiceService>(ServiceService);
+    module.get<ServiceRepository>(ServiceRepository);
+    module.get<EmployeeRepository>(EmployeeRepository);
   });
 
   it('should be defined', () => {
--- src/service/service.controller.ts
+++ src/service/service.controller.ts
@@ -16,41 +16,42 @@ import { Request } from 'express';
 
 @Controller('service')
 export class ServiceController {
-//   constructor(private readonly serviceService: ServiceService) {}
+  constructor(private readonly serviceService: ServiceService) {}
 
-//   @Public()
-//   @Get()
-//   getAllService() {
-//     return this.serviceService.getAllService();
-//   }
+  @Public()
+  @Get()
+  getAllService() {
+    return this.serviceService.getAllService();
+  }
 
-//   @Roles(Role.ADMIN, Role.OWNER)
-//   @Post()
-//   createService(
-//     @Body() createServiceDto: CreateServiceDto,
-//     @Req() req: Request,
-//   ) {
-//     return this.serviceService.createService(createServiceDto, req.user);
-//   }
+  @Roles(Role.ADMIN, Role.OWNER)
+  @Post()
+  createService(
+    @Body() createServiceDto: CreateServiceDto,
+    @Req() req: Request,
+  ) {
+    return this.serviceService.createService(createServiceDto, req.user);
+  }
 
-//   @Roles(Role.ADMIN, Role.OWNER)
-//   @Patch(':service')
-//   updateService(
-//     @Param('service') id: string,
-//     @Body() updateServiceDto: UpdateServiceDto,
-//   ) {
-//     return this.serviceService.updateService(id, updateServiceDto);
-//   }
+  @Roles(Role.ADMIN, Role.OWNER)
+  @Patch(':service')
+  updateService(
+    @Param('service') id: string,
+    @Body() updateServiceDto: UpdateServiceDto,
+    @Req() req: Request,
+  ) {
+    return this.serviceService.updateService(id, updateServiceDto, req.user);
+  }
 
-//   @Public()
-//   @Get(':service')
-//   getService(@Param('service') id: string) {
-//     return this.serviceService.getservice(id);
-//   }
+  @Public()
+  @Get(':service')
+  getService(@Param('service') id: string) {
+    return this.serviceService.getservice(id);
+  }
 
-//   @Roles(Role.OWNER)
-//   @Delete(':service')
-//   deleteService(@Param('service') id: string) {
-//     return this.serviceService.deleteService(id);
-//   }
+  @Roles(Role.OWNER)
+  @Delete(':service')
+  deleteService(@Param('service') id: string) {
+    return this.serviceService.deleteService(id);
+  }
 }
