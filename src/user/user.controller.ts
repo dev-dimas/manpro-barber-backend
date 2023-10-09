e61fd4729edb6803465891ddcaa236d57abfad98
Ridwan Efendi <ridwanefendi1903@gmail.com>
2023-10-09 15:20:37 +0700

    Authorization user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,21 +2,25 @@ import { Controller, Get, Post, Body, Patch, Param } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { Public } from '../decorator';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
+  @Public()
   @Post()
   create(@Body() createUserDto: CreateUserDto) {
     return this.userService.create(createUserDto);
   }
 
+  @Public()
   @Get(':user')
   findOne(@Param('user') id: string) {
     return this.userService.findOne(id);
   }
 
+  @Public()
   @Patch(':user')
   update(@Param('user') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.update(id, updateUserDto);
